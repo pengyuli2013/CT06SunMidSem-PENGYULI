fa41ed4ef67b85fef007ee70c384ee5b4afcbbf9
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-08-31 08:40:06 +0000

    31/8

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,13 @@
+function setup(){
+    createCanvas(600,400);
+    background(0);
+}
 
+function draw (){
+    fill("black");
+    stroke("red");
+    strokeWeight(10);
+    circle(150,200,100);
+    triangle(300,150, 250,250,350,250);
+    rect(400, 150,100, 100); 
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,10 @@
-
+function setup(){
+    createCanvas(600,600);
+    background(220);
+}
+function draw () {
+    for(let i = 1;i<11;i++){
+        fill(25.5*i);
+        circle(i*50,i*50,50);
+    }
+}
